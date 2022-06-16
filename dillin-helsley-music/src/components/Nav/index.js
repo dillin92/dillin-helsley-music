19b4c6d35092bb33d140222f574579bf662c1b7b
dillin92 <dillin970@gmail.com>
2022-06-16 14:17:51 -0400

    initial setup, created Navbar

---
+++ dillin-helsley-music/src/components/Nav/index.js
@@ -0,0 +1,40 @@
+import {FaBars} from 'react-icons/fa';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+
+
+
+
+const Navbar = () => {
+
+    return (
+     <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>
+                    Dillin Helsley Music
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars/>
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks href="#portfolio">Videos</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks href="#about">About</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks href="#contact">Contact</NavLinks>
+                    </NavItem>
+                    
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+     </>
+    )
+}
+
+
+export default Navbar;
